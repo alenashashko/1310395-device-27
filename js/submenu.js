d881ebc2d43ef2e9ea8a7cb0a3d67a0982d40184
alenashashko <L.Shashko@mail.ru>
2020-02-28 18:26:23 +0300

    Убирает ссылку на каталог товаров при работающем js

--- js/submenu.js
+++ js/submenu.js
@@ -3,6 +3,16 @@ let submenu = document.querySelector(".header-catalog-list");
 
 submenu.classList.remove("submenu-show");
 
+submenuLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+});
+
+submenuLink.addEventListener("kewdown", function (evt) {
+  if (evt.keyCode === 13) {
+  evt.preventDefault();
+  }
+});
+
 submenuLink.addEventListener("focus", function () {
     submenu.classList.toggle("submenu-show");
 });
